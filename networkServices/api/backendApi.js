0fc37b532b999048cc5ce4c60eb50537a20dddf7
MrNobody806 <manuhe839@example.com>
2025-06-01 17:29:15 +0300

    update

--- networkServices/api/backendApi.js
+++ networkServices/api/backendApi.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   // baseURL: "http://127.0.0.1:8000/api",
-<<<<<<< HEAD
-  baseURL: "http://192.168.137.153:8000/api",
-=======
   baseURL: "http://192.168.43.217:8000/api",
->>>>>>> e6c587a3662bfe54f2d3f94cbc16f6b2718b1d74
   headers: {
     "content-type": "application/json",
     withCredentials: true,
